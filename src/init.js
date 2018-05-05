dcbbfebb8c52bc0278495656087049500503d8bc
wangdesheng <737708900@qq.com>
2018-05-05 18:38:53 +0800

    增加初始化配置

--- src/init.js
+++ src/init.js
@@ -1,6 +1,15 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import GlobalComponents from './components'
+import flex from '../src/assets/lib/flexible'
+import {checkSystem} from '../src/assets/lib/utils'
+
+// 手机端处理
+(function () {
+  window.onresize = function () {
+    flex(100, 1, checkSystem().mobile)
+  }
+})()
 
 Vue.config.productionTip = false
 Vue.use(Router)
