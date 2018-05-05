d4148118ba9d43db34dc7d87a45b14decae1bfb2
wangdesheng <737708900@qq.com>
2018-05-06 03:07:04 +0800

    组件测试

--- src/pages/test/App.vue
+++ src/pages/test/App.vue
@@ -1,41 +1,21 @@
 <template>
   <div id="app">
-    <h1>Test</h1>
-    <video-player
-      source="https://v.suv666.com/material/d5025707035da6d363afc9d248e28b22.mp4"
-      min-height="14rem"
-      >
-    </video-player>
+    <test-title />
     <img src="../../assets/logo.png">
-    <local-component></local-component>
-    <router-view/>
+    <elRow :gutter="20" rowStyle='background:red'>
+      <div>111</div>
+    </elRow>
   </div>
 </template>
 
 <script>
-import VideoPlayer from '../../components/VideoPlayer'
-import HelloWorld from '../../containers/HelloWorld'
+  import UI from '../../elementUI'
 
-const localComponent = {
-  template: '<div>我是局部组件,使用const申明</div>'
-}
-
-export default {
-  name: 'App',
-  components: {VideoPlayer, localComponent, HelloWorld}
-}
+  const testTitle = {
+    template: '<H2>Test</H2>'
+  }
+  export default {
+    name: 'App',
+    components: {testTitle, ...UI}
+  }
 </script>
-
-<style>
-  body{
-    margin: 0
-  }
-  #app {
-    font-family: 'Avenir', Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-    margin: 0;
-  }
-</style>
